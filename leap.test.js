a177020a73b8a7ced5ea2884ee269a7ca0a3bb0a
Sonja Duric <duric.sonja@gmail.com>
2017-08-22 13:54:17 -0400

    Evaluates a normal leap year correctly and ignores node_modules too

--- leap.test.js
+++ leap.test.js
@@ -5,3 +5,13 @@ test("a year that is not a leap year.", function () {
     store.dispatch(changeYear(2015));
     expect(store.getState().isLeap).toBe(false);
 });
+
+test("a year that is a normal leap year.", function () {
+    store.dispatch(changeYear(2016));
+    expect(store.getState().isLeap).toBe(true);
+});
+
+test("a normal leap year is skipped every 100 years.", function () {
+    store.dispatch(changeYear(1900));
+    expect(store.getState().isLeap).toBe(false);
+});
